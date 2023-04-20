c80f8bf3707cb38ac473f6fa7f2811123993411f
anilac13 <anil4u2496@gmail.com>
2023-04-20 12:34:47 +0530

    [Anil] Add:Demo project

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoApp demo = new DemoApp();
+            demo.Add();
+        }
+    }
+}
